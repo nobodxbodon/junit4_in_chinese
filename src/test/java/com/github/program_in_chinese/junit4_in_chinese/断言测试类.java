69f4a4bef9df98a04dea5ece4e86f7f8309a2026
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-08-07 21:40:24 -0700

    汉化所有断言API

---
+++ src/test/java/com/github/program_in_chinese/junit4_in_chinese/断言测试类.java
@@ -0,0 +1,14 @@
+package com.github.program_in_chinese.junit4_in_chinese;
+
+import static com.github.program_in_chinese.junit4_in_chinese.断言.相等;
+
+import org.junit.Test;
+
+public class 断言测试类 {
+
+  @Test
+  public void test() {
+    相等(1, 1);
+  }
+
+}
